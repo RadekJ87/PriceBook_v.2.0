3ad7fe92ab6a610b399177ea948953b7d2c72b44
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2022-12-23 13:29:11 +0100

    install React Router Dom and set basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,61 @@
+import React from 'react';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
+import Footer from "./components/Footer";
+import Login from "./views/Login";
+import Products from "./views/Products";
+import Options from "./views/Options";
+import Error from "./views/Error";
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        errorElement: <Error/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>,
+            },
+            {
+                path: "/login",
+                element: <Login/>,
+            },
+            {
+                path: "/products",
+                element: <Products/>,
+            },
+            {
+                path: "/options",
+                element: <Options/>,
+            },
+
+        ]
+    },
+]);
+
+
 const App = () => {
     return (
-        <div className="App">
-            Hello React!
+        <div className="app">
+            <div className="container">
+                <RouterProvider router={router}/>
+            </div>
         </div>
     );
 }

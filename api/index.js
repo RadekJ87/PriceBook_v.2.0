267f7209911433258377d417027a6dc459725801
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2022-12-29 19:41:08 +0100

    install mogoose and dotenv, create .env file and set connection to DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import * as dotenv from 'dotenv'
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected to database'))
+    .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Welcome to Express!');

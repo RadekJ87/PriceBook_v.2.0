caf2537e63bdfc8de4dd5ff2b32e79eb27d65715
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2022-12-30 13:08:49 +0100

    create User schema

---
+++ api/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    profilePic: {
+        type: String,
+        default: '',
+    }
+}, {timestamps: true});
+
+export const User = mongoose.model('User', userSchema);

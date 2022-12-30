a1be2936513e2214b0f4fc8a55807f39247cf80e
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2022-12-30 20:14:28 +0100

    create login method -> POST to 'api/auth/login'

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,30 +1,48 @@
 import express from "express";
 import {User} from "../models/User.js";
 import bcrypt from "bcrypt";
-import * as dotenv from "dotenv";
 
 const authRouter = express.Router();
-dotenv.config();
 
 authRouter
+    .post('/register', async (req, res) => {
+        try {
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(req.body.password, salt);
 
-.post('/register', async(req, res) => {
-   try{
-       const salt = bcrypt.genSaltSync(10);
-       const hash = bcrypt.hashSync(req.body.password, salt);
+            const user = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: String(hash),
+            })
 
-       const user = new User({
-           username: req.body.username,
-           email: req.body.email,
-           password: String(hash),
-       })
+            const newUser = await user.save();
+            res.status(200).json(newUser);
 
-       const newUser = await user.save();
-       res.status(200).json(newUser);
+        } catch (error) {
+            // moze zwracać
+            // const err = error.keyValue?.username ?? error.keyValue?.email;
+            // console.log(err);
+            res.status(500).json(error)
+        }
+    })
 
-   } catch (error) {
-       res.status(500).json(error)
-   }
-})
+    .post('/login', async (req, res) => {
+        try {
+            const user = await User.findOne({username: req.body.username});
+            if (!user) return res.status(400).json('Wrong credentials');
+
+            const {password, ...others} = user._doc;
+
+            const isPasswordCorrect = await bcrypt.compare(req.body.password, password);
+            if (!isPasswordCorrect) return res.status(400).json('Wrong credentials');
+
+            return res.status(200).json(others);
+
+        } catch (error) {
+            res.status(500).json(error)
+        }
+
+    })
 
 export default authRouter;

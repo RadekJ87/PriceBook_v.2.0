7f42893644b1d866489d19e7bf698b75fea6aa3a
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2023-01-02 15:01:03 +0100

    create desktop view for products page and normal user

---
+++ client/src/components/CustomizableTableHead.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import TableRow from "@mui/material/TableRow";
+import {TableCell} from "@mui/material";
+import TableHead from "@mui/material/TableHead";
+
+const CustomizableTableHead = ({columns}) => {
+    return (
+            <TableHead>
+                <TableRow>
+                    {columns.map((column) => (
+                        <TableCell
+                            key={column.id}
+                            align={column.align}
+                            sx={{
+                                minWidth: column.minWidth,
+                                fontFamily: 'Oswald',
+                                fontSize: '16px',
+                                letterSpacing: '0.5px',
+                                backgroundColor: "#E8F0FE"}}
+                        >
+                            {column.label}
+                        </TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+    );
+};
+
+export default CustomizableTableHead;

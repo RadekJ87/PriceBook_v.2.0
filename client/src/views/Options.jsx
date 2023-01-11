d788901107f3a5990238d2824128e5bf1269a6e6
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2023-01-11 20:51:54 +0100

    initial commit for branch -> feature/adminpage, set background and basic box responsible for displaying available options

--- client/src/views/Options.jsx
+++ client/src/views/Options.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import Lathe from "../images/backgroundAdmin.avif";
+import WallpaperDiv from "../components/WallpaperDiv";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+
+
 
 const Options = () => {
+    // kafelka refactor do komponentu SingleAdminOption
     return (
-        <div>
-            Options
-        </div>
+        <WallpaperDiv image={Lathe}>
+            <Box
+                sx={{
+                    // backgroundColor: "cyan",
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
+                    '& > :not(style)': {
+                        xs: {
+                            margin: "10px",
+                            width: 165,
+                            height: 220,
+                        },
+                        sm: {
+                            margin: "15px",
+                            width: 190,
+                            height: 250,
+                        },
+                        md: {
+                            margin: "20px",
+                            width: 280,
+                            height: 400,
+                        }
+                    },
+                }}
+            >
+                <Paper elevation={3}>
+
+                </Paper>
+            </Box>
+        </WallpaperDiv>
     );
 };
 

b960023abb5903a4b78abfcfb84f244f98cd7c67
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2023-01-23 13:18:44 +0100

    create layout for UserCreator - desktop

--- client/src/components/UserCreator.jsx
+++ client/src/components/UserCreator.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
-import {Box, Fab, FormControlLabel, Input, Switch} from "@mui/material";
+import {Badge, Box, Fab, FormControlLabel, Input, Switch} from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import Avatar from "@mui/material/Avatar";
+import {styled} from "@mui/material/styles";
+import BadgeInput, {SmallCameraIconBadge} from "./BadgeInput";
+
+// test
+const avatar = "https://randomuser.me/api/portraits/men/75.jpg";
+// const avatar = "https://picsum.photos/id/237/200/300";
+const badge = "https://randomuser.me/api/portraits/thumb/men/76.jpg";
+
+
+const BigAvatar = styled(Avatar)(({ theme }) => ({
+    width: 200,
+    height: 200,
+    backgroundSize: "cover",
+    boxShadow: "4px 6px 15px 5px rgba(110,110,110,0.8)",
+}));
+
+const FormBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    padding: "10px",
+    width: "55%",  //te dwa do skalowania
+    height: "500px",
+    backgroundColor: "rgba(7, 7, 12, 0.05)",
+    border: "1px solid #cccccc",
+    boxShadow: "2px 3px 10px 0px rgba(161,161,161,0.7)",
+    transition: "all ease 0.2s",
+    borderRadius: "5px",
+    '&:hover': {
+        transform: 'translateY(-1px)',
+        boxShadow: '0px 10px 20px rgba(161,161,161,0.7)',
+    }
+}));
 
 const UserCreator = () => {
     return (
@@ -11,7 +45,7 @@ const UserCreator = () => {
             padding: "10px",
             width: "55%",  //te dwa do skalowania
             height: "500px",
-            // backgroundColor: "lightGreen",
+            backgroundColor: "rgba(7, 7, 12, 0.05)",
             border: "1px solid #cccccc",
             boxShadow: "2px 3px 10px 0px rgba(161,161,161,0.7)",
             transition: "all ease 0.2s",
@@ -35,39 +69,54 @@ const UserCreator = () => {
                     justifyContent: "center",
                     // backgroundColor: "darkGreen"
                 }}>
-                    <Box sx={{ height: "50%", backgroundColor: "red"}}>
-                        <Input name="profilePicture" placeholder="input od uploadu zdjecia"/>
-                        Zdjecie
+                    <Box sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        height: "50%",
+                        // backgroundColor: "red"
+                    }}>
+                        {/*<Input name="profilePicture" placeholder="input od uploadu zdjecia"/>*/}
+                        {/*Zdjecie*/}
+                        <Badge
+                            overlap="circular"
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                            badgeContent={<BadgeInput badgeComponent={<SmallCameraIconBadge/>}/>}
+                        >
+                            <BigAvatar className="user" alt="Travis Howard" src={avatar} />
+                        </Badge>
                     </Box>
                 </Box>
                 <Box className="data-box" sx={{
                     display: "flex",
-                    flexDirection: "column",
+                    flexDirection: "row",
                     alignItems: "left",
                     // justifyContent: "space-around",
                     flex: 3,
-                    // backgroundColor: "green"
+                    // backgroundColor: "yellow"
                 }}>
                     <Box sx={{
                         display: "flex",
                         flex: 4,
                         flexDirection: "column",
                         justifyContent: "center",
-                        gap: 4,
+                        gap: 3,
                         '&>: not(style)': {
-                            width: "60%"}
+                            marginLeft: "10px",
+                            width: "70%"}
                     }}>
                         <Input name="username" placeholder="Nazwa użytkownika"/>
                         <Input name="email" placeholder="Adres email"/>
                         <Input name="password" placeholder="Hasło"/>
-                    </Box>
-                    <Box sx={{flex: 1}}>
-                        <FormControlLabel
-                            control={
-                                <Switch checked={true} onChange={()=>console.log('switch')} name="admin" />
-                            }
-                            label="Czy dodać uprawienienia administratora?"
-                        />
+                        <Box sx={{
+                            // backgroundColor:"orange"
+                        }}>
+                            <FormControlLabel
+                                control={
+                                    <Switch checked={true} onChange={()=>console.log('switch')} name="admin" />
+                                }
+                                label="Czy dodać uprawienienia administratora?"
+                            />
+                        </Box>
                     </Box>
                 </Box>
             </Box>

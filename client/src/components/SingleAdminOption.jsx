c15517ac5b77322ed6038c446f9797e101576cc5
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2023-01-25 14:25:46 +0100

    refactor - optimize view Options for mobile and desktop

--- client/src/components/SingleAdminOption.jsx
+++ client/src/components/SingleAdminOption.jsx
@@ -5,27 +5,38 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link} from "react-router-dom";
 
 
-const SingleAdminOption = ({faIcon, text, url, primaryColor ='#fafafa', secondaryColor='slateblue', ...props}) => {
+const SingleAdminOption = ({faIcon, text, url, primaryColor = '#fafafa', secondaryColor = 'slateblue', ...props}) => {
+
     return (
-            <Paper
-                elevation={3}
-                component={Link}
-                to={url}
-                sx={{
-                    cursor: "pointer",
-                    textDecoration: "none",
-                    backgroundColor: `${secondaryColor}`,
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-evenly",
-                    alignItems: "center"
+        <Paper
+            elevation={3}
+            component={Link}
+            to={url}
+            sx={{
+                fontSize: {xs: "20px", sm: "28px", md: "32px", lg: "36px"},
+                width: {xs: "180px", sm: "190px", md: "230px", lg: "280px"},
+                height: {xs: "200px", sm: "250px", md: "320px", lg: "400px"},
+                cursor: "pointer",
+                textDecoration: "none",
+                backgroundColor: `${secondaryColor}`,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-evenly",
+                alignItems: "center"
             }}>
-                <Typography
-                    variant="h4" sx={{color: `${primaryColor}`}}>
-                    {text}
-                </Typography>
-                <FontAwesomeIcon icon={faIcon} size="5x" style={{color: `${primaryColor}`, "--fa-animation-duration": "5s"}} {...props}/>
-            </Paper>
+            <Typography
+                variant="h4" sx={{fontSize: "1em", color: `${primaryColor}`}}>
+                {text}
+            </Typography>
+            <FontAwesomeIcon
+                icon={faIcon}
+                // size="5x"
+                style={{
+                    fontSize: "2.35em",
+                    color: `${primaryColor}`,
+                    "--fa-animation-duration": "5s"
+                }} {...props}/>
+        </Paper>
     );
 };
 

408d8e083121ff91f7156b7323f78b41593035cf
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2023-01-26 20:20:26 +0100

    add span for displaying errors

--- client/src/components/ActionsBox.jsx
+++ client/src/components/ActionsBox.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import {Box, Fab} from "@mui/material";
+import {Box, Fab, Typography} from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const ActionsBox = (onClick) => {
+const ActionsBox = ({errorMessage, onCreate}) => {
     return (
-            <Box sx={{
-                fontSize: {xs: '8px', sm: "10px", md: "12px", lg: "14px"},
-                display: 'flex',
-                alignContent: "center",
-                justifyContent: "flex-end"
-            }}>
-                <Fab
-                    variant="extended"
-                    size="small"
-                    color="success"
-                    sx={{fontSize: "1.1em",}}
-                    onClick={() => console.log('Add new')}>
-                    <CheckCircleIcon sx={{fontSize: "2em", mr: 1}}/>
-                    Dodaj do bazy
-                </Fab>
-            </Box>
+        <Box sx={{
+            fontSize: {xs: '8px', sm: "10px", md: "12px", lg: "14px"},
+            display: 'flex',
+            alignItems: "center",
+            justifyContent: "space-between"
+        }}>
+            <Typography
+                variant="h6"
+                component="span"
+                sx={{
+                    color: "red",
+                    fontWeight: 400,
+                    fontSize: {xs: "0.9em", md: "0.7em", lg: "0.65em", xl: "0.8em"},
+                    marginLeft: "1em"
+                }}>
+                {errorMessage ?? null}
+            </Typography>
+            <Fab
+                variant="extended"
+                size="small"
+                color="success"
+                sx={{fontSize: {xs: "0.9em", md: "0.7em", xl: "0.8em"}}}
+                onClick={onCreate}
+            >
+                <CheckCircleIcon sx={{fontSize: {xs: "2em"}, mr: "8px"}}/>
+                Dodaj do bazy
+            </Fab>
+        </Box>
     );
 };
 
